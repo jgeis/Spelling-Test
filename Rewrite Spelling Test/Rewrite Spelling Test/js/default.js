4da5d423474598c71e3839c6577bb1ba2b7eabe5
Joseph <geis28@gmail.com>
2015-02-04 20:39:14 -0800

    Added some updates

--- Rewrite Spelling Test/Rewrite Spelling Test/js/default.js
+++ Rewrite Spelling Test/Rewrite Spelling Test/js/default.js
@@ -33,21 +33,44 @@
 var testStart = function () {
     // Get values of the text boxes
     var w1 = document.getElementById("w1").value;
+    console.log("Successfully imported w1!");
     var w2 = document.getElementById("w2").value;
+    console.log("Successfully imported w2!");
     var w3 = document.getElementById("w3").value;
+    console.log("Successfully imported w3!");
     var w4 = document.getElementById("w4").value;
+    console.log("Successfully imported w4!");
     var w5 = document.getElementById("w5").value;
+    console.log("Successfully imported w5!");
     var w6 = document.getElementById("w6").value;
+    console.log("Successfully imported w6!");
     var w7 = document.getElementById("w7").value;
+    console.log("Successfully imported w7!");
     var w8 = document.getElementById("w8").value;
+    console.log("Successfully imported w8!");
     var w9 = document.getElementById("w9").value;
+    console.log("Successfully imported w9!");
     var w10 = document.getElementById("w10").value;
+    console.log("Successfully imported w10!");
     var w11 = document.getElementById("w11").value;
+    console.log("Successfully imported w11!");
     var w12 = document.getElementById("w12").value;
+    console.log("Successfully imported w12!");
     var w13 = document.getElementById("w13").value;
+    console.log("Successfully imported w13!");
     var w14 = document.getElementById("w14").value;
+    console.log("Successfully imported w14!");
     var w15 = document.getElementById("w15").value;
+    console.log("Successfully imported w15!");
     var w16 = document.getElementById("w16").value;
-    print();
+    console.log("Successfully imported w16!");
 
+    // Hide the text boxes
+    var inputList = document.getElementById("inputList");
+    inputList.hidden = "true"
+
+    // Check the words
+    if (w1 !== "") {
+        
+    };
 }
